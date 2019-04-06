2a832f4a3d50a2a7b1b1a861d71706afeeef5bb0
David-M-Nielsen <monbergnielsen@outlook.com>
2019-04-07 01:23:07 +0200

    refactored the program into methods and stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,51 +7,57 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import java.util.Scanner;  // Import the Scanner class
+import java.util.UUID;
+import org.eclipse.paho.client.mqttv3.IMqttClient;
 
 public class Main {
 
-    public static void main(String[] args) {
-        MqttClient client = null;
-        try {
-            client = new MqttClient(
-                    "tcp://207.154.218.89", //URI
-                    MqttClient.generateClientId(), //ClientId
-                    new MemoryPersistence());
-        } catch (MqttException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    private static String publisherId = UUID.randomUUID().toString();
+    private static IMqttClient client;
 
-//                MqttConnectOptions options = new MqttConnectOptions();
-//                options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1_1);
-////                options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
-        try {
-            client.connect();
-            System.out.println("connected");
-        } catch (MqttException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        client.isConnected();
-        System.out.println("isConnected");
-        
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    public static void main(String[] args) throws MqttException {
+        client = new MqttClient("tcp://207.154.218.89:1883", publisherId);
+
+        clientConnect();
+
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object<
         System.out.println("Enter Topic");
 
-        String topic = myObj.nextLine();  // Read user input
+        String topic = scanner.nextLine();  // Read user input
         System.out.println("Topic is: " + topic);  // Output user input 
 
-        while(true){
-            System.out.println("Enter message");
-            String message = myObj.nextLine();
-        MqttMessage msg = new MqttMessage();
-        msg.setPayload(message.getBytes());
-        msg.setQos(0);
-        msg.setRetained(true);
-        try {
-            client.publish(topic, msg);
-        } catch (MqttException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        while (true) {
+            System.out.print("Enter your message: ");
+            String message = scanner.nextLine();
+
+            publisher(topic, message);
         }
+    }
+    
+    private static void clientConnect(){
+        System.out.println("Connecting...");
+        
+        while (!client.isConnected()) {
+            try {
+                client.connect();
+                System.out.print(".");
+            } catch (MqttException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+
+        System.out.println("\nConnected");
+    }
+
+    private static void publisher(String topic, String msg) throws MqttException {
+        MqttMessage message = new MqttMessage(msg.getBytes());
+
+        message.setQos(0);
+        message.setRetained(true);
+
+        client.publish(topic, message);
+
+
     }
 
 }
